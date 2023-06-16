9fcefc7b3797d896969c0e35767267ffe7d9066f
Cristian Moller <mollercristian@gmail.com>
2023-06-16 19:03:40 +0000

    Paginated with scroll infinit

--- src/components/apiStarWars.tsx
+++ src/components/apiStarWars.tsx
@@ -1,60 +1,81 @@
 import { useEffect, useState } from "react"
 import { DetailStarShip } from "./detailStarShip"
 import "../App.css"
-import Paginated from "./paginated"
 import { IStarship } from "../types/interfaces"
+import InfiniteScroll from "react-infinite-scroll-component"
 
 function ApiStarWars() {
 
   const [starShips, setStarShips] = useState<IStarship[]>([])
   const [itemStarShips, setItemStarShips] = useState<IStarship[]>([])
   const [page, setPage] = useState<number>(1)
-  const [next, setNext] = useState<number>()
+  const [next, setNext] = useState<number|null>(0)
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const url = `https://swapi.dev/api/starships/?page=${page}`
 
-  function handleNextPage(): void {
-    if(next != null) setPage(page+1);
-  }
-
-  useEffect(() => {
+  const GetStarShip = () => {
     setIsLoading(true)
+    if(next !== null) {
+      setPage(page+1);
+    } 
     fetch(url)
-      .then(res => res.json())
-      .then((res) => {
+    .then(res => res.json())
+    .then((res) => {
+      if(res.results){
         setIsLoading(false)
         setStarShips(res.results)
         setNext(res.next)
         setItemStarShips(itemStarShips.concat(res.results))
-        console.log(res.next)
-      });
-  }, [page, url])
+      }
+    })
+    console.log(next)
+  }
+
+  useEffect(() => {
+    GetStarShip()
+  }, [])
+
+  function refresh() {
+    throw new Error("Function not implemented.")
+  }
 
   return (
 
     <>
-      <ul className="listStarShips" >
-        { 
-          itemStarShips.map((elem, index) => {
-
-            return (
-              <li key={elem.name}>
-
-                <DetailStarShip
-                  key={index}
-                  ship={elem}
-                />
-              </li>
-            )
-          })
+    
+      <InfiniteScroll
+        dataLength={itemStarShips.length} //This is important field to render the next data
+        next={GetStarShip}
+        hasMore={ (next !== null) && true}
+        loader={isLoading && <h4>Loading...</h4>}
+        endMessage={ 
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
         }
+        // below props only if you need pull down functionality
+        refreshFunction={refresh}
+        pullDownToRefresh
+        pullDownToRefreshThreshold={1}
+      >
+        <ul className="listStarShips" >
+          { 
+            itemStarShips.map((elem, index) => {
 
-        <Paginated
-          handleNext={handleNextPage}
-          isLoading={isLoading}
-        />
-      </ul>
+              return (
+                <li key={elem.name}>
+
+                  <DetailStarShip
+                    key={index}
+                    ship={elem}
+                  />
+                </li>
+              )
+            })
+          }
+        </ul>
+      </InfiniteScroll>
     </>
   )
 }
